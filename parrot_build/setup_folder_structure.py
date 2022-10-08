c69c0163d0facaa75a9820770351c39c4061230e
Terrence Williams <terrence@dubtroop.com>
2022-10-07 21:17:12 -0400

    adding operarting system to the folder structure

--- parrot_build/setup_folder_structure.py
+++ parrot_build/setup_folder_structure.py
@@ -30,20 +30,24 @@ def main() -> None:
     htb_dir = os.path.abspath(args.htb_dir)
     parent_dirs: List[str] = list(args.parent_dirs)
     children_dirs: List[str] = list(args.children_dirs)
+    operating_systems: List[str] = ["linux", "windows"]
 
     if not os.path.exists(htb_dir):
         print("HackTheBox directory does not exist")
         os.mkdir(htb_dir)
 
-    for parent_dir in parent_dirs:
-        parent_dir_path = os.path.join(htb_dir, parent_dir)
-        if not os.path.exists(parent_dir_path):
-            os.mkdir(parent_dir_path)
+    os.chdir(htb_dir)
 
-        for child_dir in children_dirs:
-            child_dir_path = os.path.join(parent_dir_path, child_dir)
-            if not os.path.exists(child_dir_path):
-                os.mkdir(child_dir_path)
+    for parent_dir in parent_dirs:
+        for operating_system in operating_systems:
+            parent_dir_path = os.path.join(htb_dir, parent_dir, operating_system)
+            if not os.path.exists(parent_dir_path):
+                os.mkdir(parent_dir_path)
+
+            for child_dir in children_dirs:
+                child_dir_path = os.path.join(parent_dir_path, child_dir)
+                if not os.path.exists(child_dir_path):
+                    os.mkdir(child_dir_path)
 
 
 if __name__ == "__main__":
