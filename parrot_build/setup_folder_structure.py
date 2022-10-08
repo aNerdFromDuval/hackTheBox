f7b4f9df35d27f84e2e951b7c3a694ed70e0ca64
aNerdFromDuval <terrence@dubtroop.com>
2022-10-08 01:37:57 +0000

    removing bugs

--- parrot_build/setup_folder_structure.py
+++ parrot_build/setup_folder_structure.py
@@ -15,13 +15,13 @@ def main() -> None:
     )
     parser.add_argument(
         "--parent-dirs",
-        type=list,
+        type=str,
         help="List of parent directories to create",
         required=True,
     )
     parser.add_argument(
         "--children-dirs",
-        type=list,
+        type=str,
         help="List of child directories to create",
         required=True,
     )
@@ -34,7 +34,7 @@ def main() -> None:
 
     if not os.path.exists(htb_dir):
         print("HackTheBox directory does not exist")
-        os.mkdir(htb_dir)
+        os.makedirs(htb_dir)
 
     os.chdir(htb_dir)
 
@@ -42,12 +42,12 @@ def main() -> None:
         for operating_system in operating_systems:
             parent_dir_path = os.path.join(htb_dir, parent_dir, operating_system)
             if not os.path.exists(parent_dir_path):
-                os.mkdir(parent_dir_path)
+                os.makedirs(parent_dir_path)
 
             for child_dir in children_dirs:
                 child_dir_path = os.path.join(parent_dir_path, child_dir)
                 if not os.path.exists(child_dir_path):
-                    os.mkdir(child_dir_path)
+                    os.makedirs(child_dir_path)
 
 
 if __name__ == "__main__":
